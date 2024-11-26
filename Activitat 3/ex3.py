9a1e8cc963df88071918c833684e91395c6b15f4
Lorena Peiró <2024_lorena.peiro@iticbcn.cat>
2024-11-26 01:29:25 +0100

    Delete Activitat 3/ex3.py

--- Activitat 3/ex3.py
+++
@@ -1,7 +0,0 @@
-'''Enunciat:
-Imprimir els elements d'una llista amb for. fruits = [“poma”,”pera”,”raïm”,”plàtan”]
-'''
-fruites = ["poma","pera","raïm","plàtan"]
-    
-for element in fruites:
-    print(element)
