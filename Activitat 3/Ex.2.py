72b11e44c6f686824bb953a7ec702c454b105cf3
lorebcn84 <2024_lorena.peiro@iticbcn.cat>
2024-11-26 09:43:44 +0100

    Finalitzada Activitat 3

---
+++ Activitat 3/Ex.2.py
@@ -0,0 +1,7 @@
+'''Enunciat:
+Sumar els primers 10 números utilitzant for i range().
+'''
+suma = 0
+for i in range (1,11):
+    suma = suma + i
+print(f"La suma dels 10 primers números és : {suma}")
